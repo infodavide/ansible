fba60a4961107c671863cc65bae578f26b92d501
David R_ <36084356+infodavide@users.noreply.github.com>
2024-01-24 10:46:34 +0100

    Removal of unused files

--- roles/openvpn/files/etc/easy-rsa/build-ca.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-/usr/bin/expect <<END_EXPECT
-    spawn easyrsa --vars=./vars build-ca
-    expect "Enter New CA Key Passphrase:"
-    send "$PASSPHRASE\r"
-    expect "Re-Enter New CA Key Passphrase:"
-    send "$PASSPHRASE\r"
-    expect eof
-END_EXPECT
